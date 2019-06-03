36da7c7e94057a03d84f465cb9a97121936c789b
sgmudbidri <gmudbidri@hotmail.com>
2019-06-03 18:40:27 +0530

    install nodemon

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const app = express()
 const port = 3000
 
@@ -7,3 +9,5 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/testing', (req, res) => res.send('Test'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
